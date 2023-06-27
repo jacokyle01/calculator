8cbf624856793c045fb034e64651a9c0501f477a
Kyle Jacobson <jacokyle01@gmail.com>
2023-06-26 20:42:23 -0500

    Add button toggling

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const evaluator = document.querySelector('.evaluator');
+
 const output = document.querySelector('.output');
+const operations = document.querySelector('.operations');
+const symbols = ['-', '+', '*', '/'];
+let lastPressedType = null;
+
+window.onload = function() {
+    toggleButtons();
+}
 
 buttons.forEach(button => button.addEventListener("click", function (e) {
     const button = e.target;
+    lastPressedType = button.parentElement.id;
     const symbol = button.id;
     output.innerHTML = output.innerHTML + symbol;
+    toggleButtons();
+    if (lastPressedType == "evaluate") {
+        evaluateExpression();
+    }
 }))
+
+function toggleButtons() {
+    buttons.forEach(button => button.disabled = false)
+    if (lastPressedType == "operations" || lastPressedType == null) {
+        operators.forEach(operator => operator.disabled = true)
+        evaluator.disabled = true;
+    }
+    return;
+}
